7d6276a5292441615db8754d8f3b73206fd2d886
toleo <m.altunok@hotmail.com>
2018-07-06 21:30:09 +0300

    Optik

---
+++ Controllers/TasarimController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OptikTikForm.Controllers
+{
+    public class TasarimController : Controller
+    {
+        // GET: Tasarim
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tasarla()
+        {
+            return View();
+        }
+        public ActionResult Deneme()
+        {
+            return View();
+        }
+        public ActionResult Action()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptikTikForm.MvcApplication" Language="C#" %>
